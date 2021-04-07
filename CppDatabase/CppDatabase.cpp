8a4c2e91f9f8212a463f3fdb9c5ed802e23dcec7
anhh5l9239 <shangluke177568@126.com>
2021-04-07 10:38:29 +0800

    x64

--- CppDatabase/CppDatabase.cpp
+++ CppDatabase/CppDatabase.cpp
@@ -19,7 +19,7 @@ string genFilter()
 	//Todo: 从UI上构造过滤条件。注意处理条件格式不对的情况
 	return "";
 }
-INT CALLBACK DlgProc(HWND hwnd,UINT uMsg,WPARAM wParam,LPARAM lParam)
+INT_PTR CALLBACK DlgProc(HWND hwnd,UINT uMsg,WPARAM wParam,LPARAM lParam)
 {
 	int ctrlID;
 	static ListView* listView;
@@ -34,8 +34,7 @@ INT CALLBACK DlgProc(HWND hwnd,UINT uMsg,WPARAM wParam,LPARAM lParam)
 			listView->AddColumn("Phone",3);
 			listView->AddColumn("Email",4);
 			listView->AddColumn("Age",5);
-			//TODO: 修改用户名、密码、数据库名    
-
+			//TODO: 修改用户名、密码、数据库名
 			dal=new DAL("scott","tiger","orcl");
 		return TRUE;
 		case WM_COMMAND:
